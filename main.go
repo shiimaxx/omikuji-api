cbd51d5f051ef4926ae686bee95f05996a81ba84
shiimaxx <yoshima@shiimaxx.com>
2018-05-30 21:50:08 +0900

    Update

--- main.go
+++ main.go
@@ -8,49 +8,51 @@ import (
 	"time"
 )
 
-func newYear(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		t := time.Now()
-		if t.Month() == time.January && t.Day() >= 1 && t.Day() <= 3 {
-			v := struct {
-				Result string `json:"result"`
-			}{
-				Result: "daikichi",
-			}
-			if err := json.NewEncoder(w).Encode(v); err != nil {
-				log.Println("error:", err)
-			}
-			return
-		}
+// DefaultPort default for http server listen port
+const DefaultPort = ":8080"
+
+// OmikujiResponse omikuji-api response
+type OmikujiResponse struct {
+	ResultCode int    `json:"result_code"`
+	Result     string `json:"result"`
+}
+
+func omikuji() (int, string) {
+	t := time.Now()
+	if t.Month() == time.January && t.Day() >= 1 && t.Day() <= 3 {
+		return 0, "daikichi"
+	}
+	rand.Seed(t.UnixNano())
+	s := rand.Intn(6) + 1
+
+	switch s {
+	case 1:
+		return s, "kyo"
+	case 2, 3:
+		return s, "kichi"
+	case 4, 5:
+		return s, "chukichi"
+	case 6:
+		return s, "daikichi"
+	default:
+		return s, ""
 	}
 }
 
 // HandleOmikujiAPI omikuji api
 func HandleOmikujiAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	v := struct {
-		Result string `json:"result"`
-	}{}
-	t := time.Now().UnixNano()
-	rand.Seed(t)
-	s := rand.Intn(5) + 1
 
-	switch s {
-	case 1:
-		v.Result = "kyo"
-	case 2, 3:
-		v.Result = "kichi"
-	case 4, 5:
-		v.Result = "chukichi"
-	case 6:
-		v.Result = "daikichi"
-	}
+	var v OmikujiResponse
+	v.ResultCode, v.Result = omikuji()
+
 	if err := json.NewEncoder(w).Encode(v); err != nil {
 		log.Println("error:", err)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", newYear(HandleOmikujiAPI))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := DefaultPort
+	http.HandleFunc("/", HandleOmikujiAPI)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
