73ff0a5f4985b3ad4d679a46305856763ad0c5e8
shiimaxx <yoshima@shiimaxx.com>
2018-05-31 23:43:44 +0900

    Update test

--- main_test.go
+++ main_test.go
@@ -9,6 +9,18 @@ import (
 	"time"
 )
 
+func TestHandleOmikujiAPI(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	HandleOmikujiAPI(w, r)
+	rw := w.Result()
+	defer rw.Body.Close()
+
+	if rw.StatusCode != http.StatusOK {
+		t.Errorf("expected %d to eq %d", http.StatusOK, rw.StatusCode)
+	}
+}
+
 func TestHandleOmikujiAPIHappyNewYear(t *testing.T) {
 	getTime = func() time.Time { return time.Date(2018, time.January, 1, 0, 0, 0, 0, time.Local) }
 
